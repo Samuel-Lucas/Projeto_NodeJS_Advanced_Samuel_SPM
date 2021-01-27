60f5418f931537e6ba9e3b4a5461e39b33780567
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-01-27 11:11:45 -0300

    Estilizando campo de formulário

--- app/models/gsModel.js
+++ app/models/gsModel.js
@@ -1,12 +1,13 @@
 module.exports = function() {
 
 	this.getProdutos = function (connection, callback) {
-		connection.query('SELECT * FROM produtos', callback)
+		connection.query('SELECT * FROM produtos ORDER BY idproduto DESC', callback)
 	}
 
 	this.salvarProduto = function (categoria, descricao, preco, preco_venda, imagem, connection, callback) {
 		connection.query(
-			'INSERT INTO produtos (categoria, descricao, preco, preco_venda, imagem) VALUES (?, ?, ?, ?, ?)', [categoria, descricao, preco, preco_venda, imagem], callback)
+			'INSERT INTO produtos (categoria, descricao, preco, preco_venda, imagem) VALUES (?, ?, ?, ?, ?)', 
+			[categoria, descricao, preco, preco_venda, imagem], callback)
 	}
 
 	return this
